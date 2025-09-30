4527c13b33606232c1239ed0d9aa6c0d30ce0fc2
rguindi <raul_fv@hotmail.com>
2025-09-30 14:27:48 +0200

    cors

--- js/main.js
+++ js/main.js
@@ -47,10 +47,12 @@ async function fetchResults(carretera, pk) {
 // Obtener municipio y provincia a partir de coordenadas
 async function getMunicipioProvincia(lat, lon, maxRetries = 5, delayMs = 500) {
 
-  //const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=10&addressdetails=1`;
+  // PRODUCCIÓN: usa tu proxy en Nginx
+  const url = `/nominatim/reverse?lat=${lat}&lon=${lon}&format=json&zoom=10&addressdetails=1`;
 
-  // Para producción (Nginx proxy) evitar CORS
-  const url = `/osm-reverse/?lat=${lat}&lon=${lon}&format=json&zoom=10&addressdetails=1`;
+  // LOCAL: si quieres probar sin Nginx, descomenta la siguiente línea
+  // const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=10&addressdetails=1`;
+
 
 
   for (let attempt = 1; attempt <= maxRetries; attempt++) {
